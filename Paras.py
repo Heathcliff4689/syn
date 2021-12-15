b09124e2e988408346897ab141e7988f659d1cbb
heathcliff4689 <heathcliff4689@163.com>
2021-12-15 12:05:17 +0800

    add unsupervised module

--- Paras.py
+++ Paras.py
@@ -1,17 +1,17 @@
 class Arguements:
     def __init__(self):
         # model parameters
-        self.model = 'resNet_t_ftrl'
-        self.train = [0, 1]
+        self.model = 'resNet_t'
+        self.train = [1]
 
         # optimizer parameters   total_step = n_epochs * n_memories / observe_batch_size
         self.n_epochs = 2**4
         self.n_iter = 2*2
-        self.n_memories = 2000  # [0, 20000]
+        self.n_memories = 5000  # [0, 20000]
         self.observe_batch_size = 200
         self.batch_size = self.observe_batch_size
-        self.step = 2000  # others number n_memories / step
-        self.lr = 1e-3  # resNet_t 1e-4 resNet_att 1e-3
+        self.step = 5000  # others number n_memories / step
+        self.lr = 1e-4  # resNet_t 1e-4 resNet_att 1e-3
 
         # general experiments parameters
         self.hidden_layers = '256-128-128-64-32'
